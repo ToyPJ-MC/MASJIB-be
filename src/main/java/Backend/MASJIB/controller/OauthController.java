0dec0427f93a89b5e7145d6096728bc788c5204c
MoonSangWon <kuim75@naver.com>
2024-04-21 17:33:47 +0900

    feat : Member LogOut API add

--- src/main/java/Backend/MASJIB/controller/OauthController.java
+++ src/main/java/Backend/MASJIB/controller/OauthController.java
@@ -3,8 +3,13 @@ package Backend.MASJIB.controller;
 import Backend.MASJIB.jwt.dto.ResponseTokenDto;
 import Backend.MASJIB.jwt.provider.TokenProvider;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,15 +22,20 @@ public class OauthController {
     }
     @Operation(summary = "카카오 로그인",description = "카카오 로그인 시 refresh token으로 새로운 토큰 발급한다.")
     @GetMapping("/refresh")
-    public ResponseEntity login(@RequestParam String refreshToken){
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        if(username ==null) return ResponseEntity.status(401).body("인증되지 않은 사용자입니다.");
-        else {
-            ResponseTokenDto dto = tokenProvider.reGenerateToken(refreshToken);
-            return ResponseEntity.ok().header("Set-Cookie",CookieSetAccessTokenValue(dto)).body(dto);
-        }
+    public ResponseEntity Login(@RequestParam String refreshToken){
+        ResponseTokenDto dto = tokenProvider.reGenerateToken(refreshToken);
+        return ResponseEntity.ok().header("Set-Cookie",CookieSetAccessTokenValue(dto)).body(dto);
     }
     private String CookieSetAccessTokenValue(ResponseTokenDto dto){
-        return "accessToken="+dto.getAccessToken()+"; Max-Age="+dto.getAccessTokenExpiresIn()+"; Path=/; HttpOnly";
+        return "access-token="+dto.getAccessToken()+"; Max-Age="+dto.getAccessTokenExpiresIn()+"; Path=/; HttpOnly";
+    }
+
+    @Operation(summary = "로그아웃 기능", description = "로그인 되어있는 계정을 로그아웃 시킨다.")
+    @PostMapping("/logout")
+    @PreAuthorize("isAuthenticated()") //인증된 사용자만 해당 메소드 호출 가능
+    public ResponseEntity logout(){
+        String memberEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        tokenProvider.removeToken(memberEmail);
+        return new ResponseEntity("로그아웃 되었습니다.", HttpStatus.OK);
     }
 }
