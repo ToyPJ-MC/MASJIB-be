fc82789bc74b38ecf3b19b17d6eb877922c94afb
MoonSangWon <kuim75@naver.com>
2024-05-07 22:42:52 +0900

    feat : member get review from reviewListdto

---
+++ src/main/java/Backend/MASJIB/review/dto/ReviewListDto.java
@@ -0,0 +1,49 @@
+package Backend.MASJIB.review.dto;
+
+import Backend.MASJIB.review.entity.Review;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewListDto {
+
+    private long id;
+    private String comment;
+    private long shopId;
+    private String shopName;
+    private double rating;
+
+    private String taste;
+    private String hygiene;
+    private String kindness;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm", timezone = "Asia/Seoul")
+    private LocalDateTime createTime;
+    private List<String> paths = new ArrayList<>();
+
+    public static ReviewListDto set(Review review, List<String> path){
+        ReviewListDto dto = new ReviewListDto();
+        dto.setId(review.getId());
+        dto.setComment(review.getComment());
+        dto.setRating(review.getRating());
+
+        dto.setShopId(review.getShop().getId());
+        dto.setShopName(review.getShop().getName());
+
+        dto.setTaste(review.getTaste());
+        dto.setHygiene(review.getHygiene());
+        dto.setKindness(review.getKindness());
+        dto.setPaths(path);
+        return dto;
+    }
+}
